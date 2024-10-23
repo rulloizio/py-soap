1c698ba865e6869b762a2524d2571ecf62e65bd4
Patrizio Fanelli <patrizio.fanelli@gmail.com>
2024-10-23 20:14:35 +0200

    GenericWSDL

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import zeep
+import argparse, dotenv
+import vies
+
+def getParser():
+    pass
+
+if __name__== '__main__':
+    config = dotenv.dotenv_values(".env")
+    # t=vies.GenericWSDL(config["countryinfo"])
+    # t.getRequest()
+    # print(t.chiama_servizio('CurrencyName','EUR'))
+    
+    v= vies.Vies()
+    # v.getWSDL()
+    print(v.chiama_servizio('checkVat','IT'))
+    v.getRequest()
--- test1.py
+++
@@ -1,7 +0,0 @@
-import zeep
-
-wsdl_url = 'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL'
-client = zeep.Client(wsdl=wsdl_url)
-print(client.wsdl.dump())
-tt = client.get_type('ns0:CelsiusToFahrenheit')
-print (tt)
